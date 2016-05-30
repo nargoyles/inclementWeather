c19586961b994e79456d4a25ec4358466955dcf8
Nate Graves <natesgraves@gmail.com>
2016-05-30 12:27:43 -0400

    Not really working, but okay

---
+++ Assets/Scripts/UmbrellaScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UmbrellaScript : MonoBehaviour {
+	public float speed = 1.5f;
+
+	void Update() {
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			transform.position += Vector3.up * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.DownArrow)) {
+			transform.position += Vector3.down * speed * Time.deltaTime;
+		}
+	}
+}
