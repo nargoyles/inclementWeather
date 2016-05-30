0ca113db23fb9c8f4a8896edca0df98c4670e014
Seth Graves <sethwilliamgraves@gmail.com>
2016-05-30 12:42:24 -0400

    umbrella move script works, arm not remainging anchored...

--- Assets/Scripts/WalkScript.cs
+++ Assets/Scripts/WalkScript.cs
@@ -2,18 +2,15 @@
 using System.Collections;
 
 public class WalkScript : MonoBehaviour {
-
-	float move;
+	
 	float speed = 3f;
 	public Animator anim;
+
 	// Update is called once per frame
 	void Update () {
-		move = Input.GetAxis ("Horizontal");
-		if (Mathf.Abs (move) > 0) {
-			transform.Translate(Vector3.right * move * speed * Time.deltaTime);
-			anim.SetFloat ("speed", Mathf.Abs (move));
+		transform.Translate(Vector3.right * speed * Time.deltaTime);
+		anim.SetFloat ("speed", 1);
 
-		
-		}
+
 	}
 }
---
+++ Assets/Scripts/destroyScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destroyScript : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		Destroy (gameObject);
+
+	}
+}
