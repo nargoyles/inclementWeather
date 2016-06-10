8fc6c3006c734ad782217cdcd4558f2b05a0097d
Seth Graves <sethwilliamgraves@gmail.com>
2016-06-10 13:47:23 -0400

    rotate, translate relative to Space.World

--- Assets/moveIT.cs
+++ Assets/moveIT.cs
@@ -8,11 +8,11 @@ public class moveIT : MonoBehaviour {
 		
 		// Update is called once per frame
 		void Update () {
-				float translation = Input.GetAxis("Vertical") * speed;
-				float pitch = Input.GetAxis("Horizontal") * pitchSpeed;
-				translation *= Time.deltaTime;
-				pitch *= Time.deltaTime;
-				transform.Translate(0, translation, 0);
-				transform.Rotate(0, 0, pitch * -1);
-			}
+			float translation = Input.GetAxis("Vertical") * speed;
+			float pitch = Input.GetAxis("Horizontal") * pitchSpeed;
+			translation *= Time.deltaTime;
+			pitch *= Time.deltaTime;
+			transform.Translate(0, translation, 0, Space.World);
+			transform.Rotate(0, 0, pitch * -1, Space.World);
+		}
 	}
