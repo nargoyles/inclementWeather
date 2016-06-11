2605e51b9baf8f89d0f12ae731f57c79d1612fc1
Nate Graves <natesgraves@gmail.com>
2016-06-11 02:03:46 -0400

    Oooookay...it kind of works

--- Assets/moveIT.cs
+++ Assets/moveIT.cs
@@ -2,17 +2,10 @@
 using System.Collections;
 
 public class moveIT : MonoBehaviour {
-	
-		public float speed = 10.0F;
-		public float pitchSpeed = 100.0F;
-		
-		// Update is called once per frame
-		void Update () {
-			float translation = Input.GetAxis("Vertical") * speed;
-			float pitch = Input.GetAxis("Horizontal") * pitchSpeed;
-			translation *= Time.deltaTime;
-			pitch *= Time.deltaTime;
-			transform.Translate(0, translation, 0, Space.World);
-			transform.Rotate(0, 0, pitch * -1, Space.World);
-		}
+	public float speed = 1.0f;
+
+	void Update() {
+		var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+		transform.position += move * speed * Time.deltaTime;
 	}
+}
