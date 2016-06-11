9d808f8938372845242b87b014c8fd0ed93f201d
Nate Graves <natesgraves@gmail.com>
2016-06-11 16:53:45 -0400

    Update move script

--- Assets/moveIT.cs
+++ Assets/moveIT.cs
@@ -2,18 +2,19 @@
 using System.Collections;
 
 public class moveIT : MonoBehaviour {
-	public Vector3 min = new Vector3(-1.7f, 1.5f, 0);
+	public Vector3 min = new Vector3(-0.7f, 2.5f, 0);
 	public Vector3 max = new Vector3(0.7f, 4.5f, 0);
 	public float moveSpeed = 1.0f;
 
-	private Vector3 getContainedPosition(Vector3 movePosition, Vector3 newPosition) {
-		if (newPosition.x > min.x && newPosition
-	}
-
 	void Update() {
+		Vector3 newPosition = transform.position;
 		var movePosition = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
-		Vector3 newPosition = transform.position + movePosition * moveSpeed * Time.deltaTime;
-		Vector3 containedPosition = getContainedPosition (movePosition, newPosition);
-		transform.position = containedPosition;
+		newPosition += movePosition * moveSpeed * Time.deltaTime;
+		if (newPosition.x > min.x &&
+			newPosition.x < max.x &&
+			newPosition.y > min.y &&
+			newPosition.y < max.y) {
+			transform.position += movePosition * moveSpeed * Time.deltaTime;
+		}
 	}
 }
