cd90fc3529353be3d94d238a149258bb498e0934
Seth Graves <sethwilliamgraves@gmail.com>
2016-06-11 21:17:39 -0400

    added colliders to feet, updated WalkScript with ground check and animation transitions

--- Assets/Scripts/WalkScript.cs
+++ Assets/Scripts/WalkScript.cs
@@ -5,12 +5,46 @@ public class WalkScript : MonoBehaviour {
 	
 	float speed = 3f;
 	public Animator anim;
+	public float jumpForce = 5f;
+	bool jumping = false;
+	public float distance = 1f;
+	public Rigidbody2D beckard;
+	public LayerMask ground;
 
 	// Update is called once per frame
+	void FixedUpdate(){
+		
+		GroundCheck();
+
+	}
+
 	void Update () {
-		//transform.Translate(Vector3.right * speed * Time.deltaTime);
+
+		//auto-walking
+		transform.Translate(Vector3.right * speed * Time.deltaTime);
 		anim.SetFloat ("speed", 1);
 
+		//jumping
+		if(Input.GetKey(KeyCode.Space) && !jumping){
+			beckard.AddForce(new Vector2(0, jumpForce));
+		}
 
+	}
+
+	void GroundCheck()
+	{
+		RaycastHit hit;
+		Vector3 dir = new Vector3(0, -1);
+
+		if(beckard.IsTouchingLayers(ground))
+		{
+			jumping = false;
+			anim.SetBool ("jump", false);
+		}
+		else
+		{
+			jumping = true;
+			anim.SetBool ("jump", true);
+		}
 	}
 }
