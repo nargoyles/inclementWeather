21d03610300ac9ccd531346adec9a9f5d21cafed
Seth Graves <sethwilliamgraves@gmail.com>
2016-06-14 18:21:38 -0400

    please bless the prefab doesn't break

--- Assets/Scripts/WalkScript.cs
+++ Assets/Scripts/WalkScript.cs
@@ -2,40 +2,29 @@
 using System.Collections;
 
 public class WalkScript : MonoBehaviour {
-	
+
 	float speed = 3f;
 	public Animator anim;
-	public Vector2 jumpForce = new Vector2(0, 10);
-	bool jumping = false;
+	public Vector2 jumpForce = new Vector2(0, 5);
+	public bool grounded;
 	public float distance = 1f;
 	public Rigidbody2D beckardHolder;
-	public LayerMask ground;
+	public Transform beckardHolderTrans;
+	public Transform groundCheck;
 	public Vector2 velocity;
 
-	void FixedUpdate(){		
-		GroundCheck();
-		if(Input.GetKey(KeyCode.Space) || Input.GetMouseButtonDown(0) && !jumping){
-			Debug.Log(beckardHolder.velocity);
-			anim.CrossFade ("jump", 1f);
-			beckardHolder.AddForce(jumpForce, ForceMode2D.Impulse);
-		}
-	}
-
 	void Update () {
-		transform.Translate(speed * Time.deltaTime, 0, 0);
+		beckardHolderTrans.Translate(speed * Time.deltaTime, 0, 0);
 		anim.SetFloat("speed", 1);
+		grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("ground"));
+		if(Input.GetKey(KeyCode.Space) && grounded || Input.GetMouseButtonDown(0) && grounded){
+			jump();
+		}
 	}
 
-	void GroundCheck() {
-		if(beckardHolder.IsTouchingLayers(ground))
-		{
-			jumping = false;
-			Debug.Log ("Not touching");
-		}
-		else
-		{
-			jumping = true;
-			Debug.Log ("Touching");
-		}
+	void jump() {
+		anim.CrossFade ("jump", 1f);
+		beckardHolder.AddForce(jumpForce, ForceMode2D.Impulse);
 	}
+
 }
