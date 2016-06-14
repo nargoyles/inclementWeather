315c1a8d9c96f8279cebc522ea868ff17e213353
Nate Graves <natesgraves@gmail.com>
2016-06-14 01:31:03 -0400

    Fixed the animation, but physics is still elusive

--- Assets/Scripts/WalkScript.cs
+++ Assets/Scripts/WalkScript.cs
@@ -5,30 +5,30 @@ public class WalkScript : MonoBehaviour {
 	
 	float speed = 3f;
 	public Animator anim;
-	public float jumpForce = 5f;
+	public Vector2 jumpForce = new Vector2(0, 10);
 	bool jumping = false;
 	public float distance = 1f;
 	public Rigidbody2D beckard;
 	public LayerMask ground;
+	public Vector2 velocity;
 
 	// Update is called once per frame
 	void FixedUpdate(){
 		
-		GroundCheck();
-
+		//GroundCheck();
+		if(Input.GetKey(KeyCode.Space) || Input.GetMouseButtonDown(0)){
+			Debug.Log(beckard.velocity);
+			anim.CrossFade ("jump", 0f);
+			//beckard.AddForce(new Vector2(0, 100));
+			//transform.Translate(Vector3.up * 260 * Time.deltaTime, Space.World);
+			GetComponent<Rigidbody2D>().AddForce(jumpForce, ForceMode2D.Impulse);
+		}
 	}
 
 	void Update () {
-
 		//auto-walking
-		transform.Translate(new Vector3((speed * Time.deltaTime), 0, 0));
-		anim.SetFloat ("speed", 1);
-
-		//jumping
-		if(Input.GetKey(KeyCode.Space) && !jumping){
-			beckard.AddForce(new Vector2(0, jumpForce));
-		}
-
+		transform.Translate(speed * Time.deltaTime, 0, 0);
+		anim.SetFloat("speed", 1);
 	}
 
 	void GroundCheck()
