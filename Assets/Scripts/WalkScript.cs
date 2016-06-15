3686cb3d530325be3cf9f6496473ba2451ed3e68
Nate Graves <nate@tailorbrands.com>
2016-06-15 13:09:24 -0400

    Clean up

--- Assets/Scripts/WalkScript.cs
+++ Assets/Scripts/WalkScript.cs
@@ -3,28 +3,31 @@ using System.Collections;
 
 public class WalkScript : MonoBehaviour {
 
-	float speed = 3f;
+	public float speed = 3f;
 	public Animator anim;
 	public Vector2 jumpForce = new Vector2(0, 5);
 	public bool grounded;
 	public float distance = 1f;
-	public Rigidbody2D beckardHolder;
-	public Transform beckardHolderTrans;
+	public Rigidbody2D beckardBody;
+	public Transform beckardTransform;
 	public Transform groundCheck;
 	public Vector2 velocity;
 
 	void Update () {
-		beckardHolderTrans.Translate(speed * Time.deltaTime, 0, 0);
-		anim.SetFloat("speed", 1);
 		grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("ground"));
-		if(Input.GetKey(KeyCode.Space) && grounded || Input.GetMouseButtonDown(0) && grounded){
+		if (grounded) {
+			anim.SetBool ("jump", false);
+		}
+		beckardTransform.Translate(speed * Time.deltaTime, 0, 0);
+		anim.SetFloat("speed", 1);
+		if((Input.GetKey(KeyCode.Space) || Input.GetMouseButtonDown(0)) && grounded){
 			jump();
 		}
 	}
 
 	void jump() {
-		anim.CrossFade ("jump", 1f);
-		beckardHolder.AddForce(jumpForce, ForceMode2D.Impulse);
+		anim.CrossFade ("jump", 0f);
+		beckardBody.AddForce(jumpForce, ForceMode2D.Impulse);
 	}
 
 }
