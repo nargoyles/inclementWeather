41ac7b0c65a075f59f59029738af5fa678bb4e4c
Nate Graves <natesgraves@gmail.com>
2016-06-22 00:26:46 -0400

    New scene, new background

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -5,9 +5,11 @@ public class CameraScript : MonoBehaviour {
 
 	public Transform character;
 	public Vector3 cameraPosition;
+	public float xAdjustment = 0;
+	public float yAdjustment = 0;
 
 	void Update () {
-		cameraPosition = new Vector3 (character.position.x, 0, 0);
+		cameraPosition = new Vector3(character.position.x + xAdjustment, yAdjustment, 0);
 		transform.position = cameraPosition;
 	}
 }
